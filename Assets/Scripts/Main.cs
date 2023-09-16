82b702821fdc99952a2db5339c70a6b09850a325
harutiro <harutiro2027@outlook.jp>
2023-09-17 03:02:31 +0900

    add: Unityで加速度を取得できるようになった。

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Text text;
+    
     void Start()
     {
-        
+        if(Accelerometer.current != null) InputSystem.EnableDevice(Accelerometer.current);
     }
-
     // Update is called once per frame
     void Update()
     {
+        if (Accelerometer.current != null)
+        {
+            text.text = "Attitude: " + Accelerometer.current.acceleration.ReadValue().ToString();   
+            transform.position = Accelerometer.current.acceleration.ReadValue();
+        }
+        else
+        {
+            text.text = "Attitude: No Attitude Sensor";
+        }
         
     }
 }
